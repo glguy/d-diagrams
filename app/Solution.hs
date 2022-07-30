07c39432c41eea4d42805212a991640796c8f2c0
Eric Mertens <emertens@gmail.com>
2022-07-30 14:59:06 -0700

    test solutions for connectedness

--- app/Solution.hs
+++ app/Solution.hs
@@ -6,25 +6,30 @@ Description:  Functions for puzzle solutions.
 module Solution (solutionExists, validPuzzle) where
 
 import Prelude hiding (all, (&&), (||), not, any, and, or)
+import Control.Monad (replicateM)
 import Data.Map qualified as Map
 import Data.Traversable (for)
 import Ersatz
     ( assert,
       exists,
       Bit,
-      Boolean((&&), (==>), (||), false, not, any, all),
+      Boolean((&&), (==>), (||), false, not, any, all, and, or),
       Codec(encode),
-      Equatable((/==)),
-      MonadSAT )
+      Equatable((===), (/==)),
+      MonadSAT,
+      (<?),
+      Bits(Bits)
+      )
 import Puzzle
     ( Puzzle(clueRows),
       Elt(C, O, M),
       rows,
       isPath,
       isChest,
-      clueCols )
+      clueCols,
+      topClues )
 import Grids (cardinal, gridMap, region3, twoArc, walls3)
-import Counting (exactly, atLeast)
+import Counting (exactly, atLeast, atMost)
 
 -- | Predicate for solved puzzles.
 validPuzzle :: Boolean a => Puzzle a -> a
@@ -60,6 +65,38 @@ validCells p = all (uncurry validCell) (Map.assocs m)
 
     no2x2 c = not (any (all isPath) (traverse at (twoArc c)))
 
+-- | Check that all the paths in a puzzle solution are connected.
+--
+-- This is achieved by assigning each path a number and checking
+-- that every path is touching a smaller number. Only one square
+-- is allowed to be assigned the special zero number, which does
+-- not need to be connected to anything else.
+connected :: MonadSAT s m => Puzzle Bit -> m ()
+connected p =
+ do -- count up expected number of open spaces
+    let n = length (concat (rows p)) - sum (topClues p)
+
+    -- pick a number of bits large enough to allow every path to have a unique number
+    let lg_n = bitsNeeded n
+
+    -- assign each location a number
+    m <- for (gridMap (rows p)) \e ->
+     do i <- Bits <$> replicateM lg_n exists
+        pure (e,i)
+
+    -- at most one path can be assigned index 0
+    assert (atMost 1 [isPath e && 0 === i | (e,i) <- Map.elems m])
+    
+    -- every path element is adjacent to a smaller path element or is the zero element
+    assert (and [isPath e1 ==>
+                 0 === i1 || or [isPath e2 && i2 <? i1 | c <- cardinal k, Just (e2,i2) <- [Map.lookup c m]]
+                | (k,(e1,i1)) <- Map.assocs m])
+
+bitsNeeded :: Int -> Int
+bitsNeeded n
+  | n < 2 = 0
+  | otherwise = 1 + bitsNeeded ((n+1) `div` 2)
+
 -- | Generate a solution to the input puzzle.
 solutionExists ::
   MonadSAT s m =>
@@ -69,4 +106,5 @@ solutionExists ::
 solutionExists old p =
  do b <- for p \_ -> exists
     assert (validPuzzle b && all (\o -> encode o /== b) old)
+    connected b
     return b
